8cd46cd25fdd834518f0edc2da18cfd24f95f03f
Niv Veertu <niv-veertu@nivs-mbp.mynet>
2019-10-06 17:16:12 +0300

    Fixed handling of empty template tag input on runner registration CLOUD-361

--- commands/register.go
+++ commands/register.go
@@ -132,7 +132,11 @@ func (s *RegisterCommand) askAnka() {
 	s.Anka.ControllerAddress = s.ask("anka-controller-address", "Please enter the Anka Cloud Controller address")
 	s.Anka.ImageId = s.ask("anka-image-id", "Please enter the Anka Image id")
 	tag := s.ask("anka-tag", "Pleas enter the tag to use (leave empty for latest)", true)
-	s.Anka.Tag = &tag
+	if tag == "" {
+		s.Anka.Tag = nil
+	} else {
+		s.Anka.Tag = &tag
+	}
 }
 
 
