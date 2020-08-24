e4f9095daf6e8574ae01bac7188917f1a458f77b
Nathan Pierce <connarpierce@gmail.com>
2020-08-24 09:24:49 -0400

    cover 1 to disable cache in tests added build-and-tar script

---
+++ build-and-tar.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VERSION=$(cat $ROOT_DIR/VERSION | cut -d/ -f2)
+cd $ROOT_DIR
+# Build binaries
+echo "Building binaries..."
+make runner-bin
+for arch in linux-amd64 linux-386 darwin-amd64; do
+  mkdir -p $ROOT_DIR/out/archived_binaries
+  cp $ROOT_DIR/out/binaries/anka-gitlab-runner-$arch $ROOT_DIR/out/archived_binaries/
+  pushd $ROOT_DIR/out/archived_binaries
+  echo "Creating tar.gz for $arch binary..."
+  rm -f anka-gitlab-runner-$arch.tar.gz
+  tar -czvf anka-gitlab-runner-$arch.tar.gz anka-gitlab-runner-$arch
+  rm -f anka-gitlab-runner-$arch
+  popd
+done
